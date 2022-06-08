038bfdec1bed42a82e644412b5a91156c3172b38
Alan Abed <alanabed@Alans-MacBook-Air.local>
2022-06-07 19:03:01 -0700

    linked script.js to html and added hours var, createPlanner function

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ var currentDay = moment().format("dddd, MMMM Do");
 var plansContainer = document.getElementById("plans-container");
 $("#currentDay").html(currentDay);
 
+var hours = [9 , 10, 11, 12, 13, 14, 15, 16, 17]
+function createPlanner(start, end) {
+    //var currentHour = moment().hours();
+    var currentHour = hour;
+    for (var hour of hours) {
+        var stHour = getStandardHour(hour);
+        var plan = document.createElement("div");
+        plan.setAttribute("class", "plan row");
+        plan.setAttribute("id", "plan-" + stHour);
+
+    }}
+
+    createPlanner();
