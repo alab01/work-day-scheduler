9551a68bb84fc8c047be4bffec7f15bc342cf8e8
Alan Abed <alanabed@Alans-MacBook-Air.local>
2022-06-07 20:12:35 -0700

    added plans-container to html for div class, added styling to black text in css, added if/esle statemnt to get AM/PM on js, added save text for save btn on js, and added funtion to convert military time to standardtime for app

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ $("#currentDay").html(currentDay);
 
 var hours = [9 , 10, 11, 12, 13, 14, 15, 16, 17]
 function createPlanner(start, end) {
-    //var currentHour = moment().hours();
-    var currentHour = hour;
+    var currentHour = moment().hours();
     for (var hour of hours) {
         var stHour = getStandardHour(hour);
         var plan = document.createElement("div");
@@ -15,14 +14,20 @@ function createPlanner(start, end) {
         var hr = document.createElement("div");
         hr.setAttribute("class", "hour col-1");
         hr.setAttribute("id", "hour-" + stHour);
-        
+        if(hour < 12)
+            $(hr).text(stHour + "AM");
+        else
+            $(hr).text(stHour + "PM");
+
         var task = document.createElement("textarea");
         task.setAttribute("class", "task col-10 future");
         task.setAttribute("class", "task col-10 present");
         task.setAttribute("class", "task col-10 past");
         task.setAttribute("id", "task-" + stHour);
 
+
         var saveBtn = document.createElement("div");
+        $(saveBtn).text("SAVE");
         saveBtn.setAttribute("class", "save-btn col-1 saveBtn");
         saveBtn.setAttribute("id", "save-btn-" + stHour);
         saveBtn.setAttribute("targetId", stHour);
@@ -33,7 +38,15 @@ function createPlanner(start, end) {
 
         plansContainer.appendChild(plan);
 
+    }
+}
 
-    }}
 
-    createPlanner();
+function getStandardHour(militaryHour) {
+    var output = militaryHour % 12;
+    if (output === 0)
+        return 12;
+    return output;
+}
+
+createPlanner();
